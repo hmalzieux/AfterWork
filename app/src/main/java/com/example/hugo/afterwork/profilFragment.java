9031bc9cc919724eae777db72e1b7a0c4acfbe9b
Unknown <hugo.malzieux@wanadoo.fr>
2017-04-30 20:00:24 +0200

    Ajout du fragment profil

--- app/src/main/java/com/example/hugo/afterwork/profilFragment.java
+++ app/src/main/java/com/example/hugo/afterwork/profilFragment.java
@@ -7,46 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link profilFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link profilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class profilFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String[] matieres = {"Android", "TEST", "Compilation", "Ingé Log." , "TEST2"};
 
     private OnFragmentInteractionListener mListener;
 
     public profilFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static profilFragment newInstance(String param1, String param2) {
         profilFragment fragment = new profilFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,17 +27,19 @@ public class profilFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+        TextView liste_matieres = (TextView) view.findViewById(R.id.matieres);
+        String s = "";
+        for (int i = 0;i<matieres.length;i++) {
+            s += matieres[i]+"\n";
+        }
+        liste_matieres.setText(s);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -77,12 +52,6 @@ public class profilFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-       /* if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
     }
 
     @Override
@@ -90,17 +59,6 @@ public class profilFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
